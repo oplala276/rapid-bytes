6991992006635750bfdc11b89063ced675b74497
Oplala <oplala2020@gmail.com>
2023-06-07 16:57:44 +0530

    last changes

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const URL = 'https://api-news-fbwj.onrender.com/';
 export const getNews = async () => {
     try {
-        return await axios.get(`${URL}/news`);
+        return await axios.get(`${URL}news`);
     } catch (error) {
         console.log('Error while calling get news api', error);
     }
