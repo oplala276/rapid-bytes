8f8beb4858912fafa1b14cfad8653d25dbfdb1ec
Oplala <oplala2020@gmail.com>
2023-06-07 15:30:10 +0530

    change in url

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const URL = 'https://news-api-g0c0.onrender.com/';
 export const getNews = async () => {
     try {
-        return await axios.get(`${URL}/news`)
+        return await axios.get(URL)
     } catch (error) {
         console.log('Error while calling get news api', error);
     }
